14d4c4898d79f6615594fe57d5af1b03b68f3b4d
Anlex1 <angel.11jase@gmail.com>
2025-04-10 23:20:12 -0500

    Ajuste final

--- TIA 3/proyectos_pia/server/routes/tipo-proyecto.js
+++ TIA 3/proyectos_pia/server/routes/tipo-proyecto.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const nuevoProyecto = await proyectosRepository.create(req.body);
-    res.status(201).json({ 
+    res.status(200).json({ 
       message: 'Proyecto creado exitosamente',
       proyecto: nuevoProyecto
     });
